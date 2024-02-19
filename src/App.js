13404b7b04c6bb31bddf2c0ab435d2554df10096
Rohinikiet <rohoniudi@gmail.com>
2024-02-19 17:40:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Navbar from './components/Navbar/Navbar';
+import Sidebar from './components/Sidebar/Sidebar';
+import JobCards from './components/Cards/JobCards'; // Import the JobCards component
 import './App.css';
+import SavedJobTable from './components/Table/SavedJobTable';
+import PieCharts from './components/Piecharts/PieCharts';
+import ResumeForm from './components/Form/ResumeForm'
+import Footer from './components/Footer/Footer'
 
 function App() {
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!isSidebarOpen);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <Navbar toggleSidebar={toggleSidebar} isSidebarOpen={isSidebarOpen} />
+      <Sidebar isSidebarOpen={isSidebarOpen} closeSidebar={toggleSidebar} />
+      <main className="content">
+        <JobCards /> 
+        <SavedJobTable />
+        <PieCharts />
+        <ResumeForm />
+        <Footer />{/* Use ResumeForm here */}
+        {/* Any other components or content can be added here */}
+        
+      </main>
     </div>
   );
 }
